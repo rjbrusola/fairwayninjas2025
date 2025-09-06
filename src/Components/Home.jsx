0aadf72b71350918eb0c9d59647cd7c9abcad59f
MelBru <rjbrusola@gmail.com>
2025-09-06 17:30:08 -0500

    Championship Round 1

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,138 @@ import React from 'react'
 export default function Home() {
     return (
         <div className="App">
-        <div className="bgroundimg" >
+        <div className="bgroundimg" width="100%" >
+            <div align="center">
+                <table width="100%" className="leaderboard">
+                    <tbody>
+                    <tr>
+                        <th colSpan="7" className="hdcpth">2025 Leaderboard</th>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <th>Ninja</th>
+                        <th>Gross</th>
+                        <th>HDCP</th>
+                        <th>Net</th>
+                        <th>Start</th>
+                        <th></th>
+                    </tr>
+                    <tr className="hdcptr">
+                        <td width="5%">1</td>
+                        <td width="14%"><font style={{color:'blue'}}>Abner</font></td>
+                        <td width="14%" align="center">82</td>
+                        <td width="14%" align="center">8</td>
+                        <td width="14%" align="center">+3</td>
+                        <td width="14%" align="center">-15</td>
+                        <td width="23%" align="right">
+                            <font className="redbold">
+                                12 under
+                            </font>
+                        </td>
+                    </tr>
+                    <tr className="hdcptr">
+                        <td width="5%">2</td>
+                        <td width="14%"><font style={{color:'blue'}}>Tony</font></td>
+                        <td width="14%" align="center">86</td>
+                        <td width="14%" align="center">13</td>
+                        <td width="14%" align="center">+2</td>
+                        <td width="14%" align="center">-13</td>
+                        <td width="23%" align="right">
+                            <font className="redbold">11 under</font></td>
+                    </tr>
+                    <tr className="hdcptr">
+                        <td width="5%">3</td>
+                        <td width="14%"><font style={{color:'blue'}}>Abe</font></td>
+                        <td width="14%" align="center">93</td>
+                        <td width="14%" align="center">22</td>
+                        <td width="14%" align="center">even</td>
+                        <td width="14%" align="center">-8</td>
+                        <td width="23%" align="right">
+                            <font className="redbold">8 under</font></td>
+                    </tr>
+                    <tr className="hdcptr">
+                        <td width="5%">4</td>
+                        <td width="14%"><font style={{color:'blue'}}>Mel</font></td>
+                        <td width="14%" align="center">98</td>
+                        <td width="14%" align="center">21</td>
+                        <td width="14%" align="center">+6</td>
+                        <td width="14%" align="center">-11</td>
+                        <td width="23%" align="right">
+                            <font className="redbold">5 under</font></td>
+                    </tr>
+                    <tr className="hdcptr">
+                        <td width="5%">5</td>
+                        <td width="14%"><font style={{color:'blue'}}>Nes</font></td>
+                        <td width="14%" align="center">86</td>
+                        <td width="14%" align="center">13</td>
+                        <td width="14%" align="center">+2</td>
+                        <td width="14%" align="center">-4</td>
+                        <td width="23%" align="right">
+                            <font className="redbold">2 under</font></td>
+                    </tr>
+                    <tr className="hdcptr">
+                        <td width="5%">6</td>
+                        <td width="14%"><font style={{color:'blue'}}>Lito</font></td>
+                        <td width="14%" align="center">94</td>
+                        <td width="14%" align="center">13</td>
+                        <td width="14%" align="center">+10</td>
+                        <td width="14%" align="center">-10</td>
+                        <td width="23%" align="right">
+                            <font>even</font></td>
+                    </tr>
+                    <tr className="hdcptr">
+                        <td width="5%">7</td>
+                        <td width="14%"><font style={{color:'blue'}}>Tom</font></td>
+                        <td width="14%" align="center">91</td>
+                        <td width="14%" align="center">11</td>
+                        <td width="14%" align="center">+9</td>
+                        <td width="14%" align="center">-6</td>
+                        <td width="23%" align="right">
+                            <font>3 over</font></td>
+                    </tr>
+                    <tr className="hdcptr">
+                        <td width="5%">8</td>
+                        <td width="14%"><font style={{color:'blue'}}>Jon</font></td>
+                        <td width="14%" align="center">91</td>
+                        <td width="14%" align="center">12</td>
+                        <td width="14%" align="center">8</td>
+                        <td width="14%" align="center">-4</td>
+                        <td width="23%" align="right">
+                            <font className="">4 over</font></td>
+                    </tr>
+                    <tr className="hdcptr">
+                        <td width="5%">T9</td>
+                        <td width="14%"><font style={{color:'blue'}}>Boyet</font></td>
+                        <td width="14%" align="center">102</td>
+                        <td width="14%" align="center">21</td>
+                        <td width="14%" align="center">10</td>
+                        <td width="14%" align="center">-4</td>
+                        <td width="23%" align="right">
+                            <font className="">6 over</font></td>
+                    </tr>
+                    <tr className="hdcptr">
+                        <td width="5%">T9</td>
+                        <td width="14%"><font style={{color:'blue'}}>Jim</font></td>
+                        <td width="14%" align="center">97</td>
+                        <td width="14%" align="center">16</td>
+                        <td width="14%" align="center">10</td>
+                        <td width="14%" align="center">-4</td>
+                        <td width="23%" align="right">
+                            <font className="">6 over</font></td>
+                    </tr>
+                    <tr className="hdcptr">
+                        <td width="5%">T9</td>
+                        <td width="14%"><font style={{color:'blue'}}>Vedel</font></td>
+                        <td width="14%" align="center">96</td>
+                        <td width="14%" align="center">15</td>
+                        <td width="14%" align="center">10</td>
+                        <td width="14%" align="center">-4</td>
+                        <td width="23%" align="right">
+                            <font className="">6 over</font></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
             <div className="h1home">
                 <h1><label style={{color:"navajowhite"}}>2025 Fairway Ninjas Golf League</label></h1>
                 <br/><br/>
